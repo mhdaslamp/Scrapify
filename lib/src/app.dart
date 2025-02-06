892aee3f91cdf8f51d9b4e16d4fd619652acf043
Fahad <mofahadkp@proton.me>
2025-02-06 23:47:39 +0530

    Added welcome and forgot password page

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,85 +1,83 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
 
-import 'sample_feature/sample_item_details_view.dart';
-import 'sample_feature/sample_item_list_view.dart';
-import 'settings/settings_controller.dart';
-import 'settings/settings_view.dart';
+
 
 /// The Widget that configures your application.
 class MyApp extends StatelessWidget {
   const MyApp({
     super.key,
-    required this.settingsController,
   });
-
-  final SettingsController settingsController;
 
   @override
   Widget build(BuildContext context) {
-    // Glue the SettingsController to the MaterialApp.
-    //
-    // The ListenableBuilder Widget listens to the SettingsController for changes.
-    // Whenever the user updates their settings, the MaterialApp is rebuilt.
-    return ListenableBuilder(
-      listenable: settingsController,
-      builder: (BuildContext context, Widget? child) {
-        return MaterialApp(
-          // Providing a restorationScopeId allows the Navigator built by the
-          // MaterialApp to restore the navigation stack when a user leaves and
-          // returns to the app after it has been killed while running in the
-          // background.
-          restorationScopeId: 'app',
+    
 
-          // Provide the generated AppLocalizations to the MaterialApp. This
-          // allows descendant Widgets to display the correct translations
-          // depending on the user's locale.
-          localizationsDelegates: const [
-            AppLocalizations.delegate,
-            GlobalMaterialLocalizations.delegate,
-            GlobalWidgetsLocalizations.delegate,
-            GlobalCupertinoLocalizations.delegate,
+    return MaterialApp(
+      
+      home: ForgetPasswordPage()
+      );
+    }
+}
+
+
+class WelcomePage extends StatelessWidget {
+  const WelcomePage({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Column(
+        children:  [
+          Container(height: MediaQuery.of(context).size.height/2, decoration: BoxDecoration(
+            gradient: LinearGradient( colors: [
+              Colors.white,
+              Colors.green
+            ],begin: AlignmentDirectional.topCenter, end: Alignment.bottomCenter)
+          ),),
+          Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+  crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text("ScrapiFy", style: TextStyle(fontSize: 40, color: Colors.teal),),
+            TextButton(onPressed: null,style: ButtonStyle(backgroundColor: WidgetStateProperty.all<Color>(Colors.green),),child: Text("Login", style: TextStyle(fontSize: 15, color: Colors.white),)),
+            TextButton(onPressed: null, style: ButtonStyle(side: WidgetStateProperty.all<BorderSide>(BorderSide(width: 1, color: Colors.blueGrey) )), child: Text("Register", style: TextStyle(fontSize: 15, color: Colors.blueGrey),))
+
           ],
-          supportedLocales: const [
-            Locale('en', ''), // English, no country code
-          ],
-
-          // Use AppLocalizations to configure the correct application title
-          // depending on the user's locale.
-          //
-          // The appTitle is defined in .arb files found in the localization
-          // directory.
-          onGenerateTitle: (BuildContext context) =>
-              AppLocalizations.of(context)!.appTitle,
-
-          // Define a light and dark color theme. Then, read the user's
-          // preferred ThemeMode (light, dark, or system default) from the
-          // SettingsController to display the correct theme.
-          theme: ThemeData(),
-          darkTheme: ThemeData.dark(),
-          themeMode: settingsController.themeMode,
-
-          // Define a function to handle named routes in order to support
-          // Flutter web url navigation and deep linking.
-          onGenerateRoute: (RouteSettings routeSettings) {
-            return MaterialPageRoute<void>(
-              settings: routeSettings,
-              builder: (BuildContext context) {
-                switch (routeSettings.name) {
-                  case SettingsView.routeName:
-                    return SettingsView(controller: settingsController);
-                  case SampleItemDetailsView.routeName:
-                    return const SampleItemDetailsView();
-                  case SampleItemListView.routeName:
-                  default:
-                    return const SampleItemListView();
-                }
-              },
-            );
-          },
-        );
-      },
-    );
+        ),]
+        )
+   
+      );
+    
   }
 }
+
+class ForgetPasswordPage extends StatelessWidget {
+  const ForgetPasswordPage({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Center(
+        child:
+          Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text("Forgot Password?", style: TextStyle(fontSize: 30, color: Colors.teal),),
+              Text("Don't worry! It occurs. Please enter the email address linked with your account.", style: TextStyle(fontSize: 16, color: Colors.blueGrey),),
+              InputChip(label: Text("Enter Your Email")),
+              TextButton(onPressed: null, style: ButtonStyle(backgroundColor: WidgetStateProperty.all<Color>(Colors.teal )), child: Text("Send Code", style: TextStyle(fontSize: 15, color: Colors.white),))
+
+            ],
+        ),
+        )
+   
+      );
+    
+  }
+}
+
